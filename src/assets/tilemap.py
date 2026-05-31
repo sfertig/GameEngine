d437a98f3fa4fba79008cb84cf40b90fc7a3e865
Sam Fertig <sfertig007@gmail.com>
2026-05-31 16:44:11 -0400

    moven __gen_collision_shapes into seperate file

--- src/assets/tilemap.py
+++ src/assets/tilemap.py
@@ -6,7 +6,7 @@ from ..basics.camera import Camera
 from ..basics.input import Keys
 from ..physics.collisions import CollisionRect
 
-from ..helpers._tilemap_files import __saveTileMap_json__, __loadTileMap_json__
+from ..helpers._tilemap_files import __saveTileMap_json__, __loadTileMap_json__, __gen_collision_shapes
 
 #tilemap collision types
 COL_FULL = 1
@@ -46,71 +46,9 @@ class Tilemap:
         change_layer(self, new_layer, self.layer)
         self.layer = new_layer
 
+    
     def __gen_collision_shapes(self):
-        os.system("cls")
-        print("Generating collision shapes...")
-        [t.Del() for t in self.collisions]
-        self.collisions.clear()
-        
-        # Separate tiles into shapes
-        full = []
-        
-        for key, value in self.tiles.items():
-            if value not in self.collDef: 
-                continue
-            # CORRECTION: Appending 'key' (grid pos) instead of 'value' (tile type)
-            if self.collDef[value] == COL_FULL:
-                full.append(key)
-
-        # Gen collision shapes
-        used = []
-        rows: list[CollisionRect] = []
-        for pos in full:
-            if pos in used: continue
-            #else creat new collision and run search
-            col = CollisionRect(pos[0]*self.width, pos[1]*self.height, self.width, self.height, self.collisionLayers)
-            #run search through tiles
-            for i in range(SEARCH_TIME):
-                for tile in full:
-                    if tile in used: continue
-                    #else check if it can be added to col shape
-                    #if to the left
-                    if tile[0] == (col.x//self.width)-1 and tile[1] == pos[1]:
-                        col.x -= self.width
-                        col.width += self.width
-                        used.append(tile)
-                    #else check if it is to the right
-                    if tile[0] == (col.x+col.width)//self.width and tile[1] == pos[1]:
-                        col.width += self.width
-                        used.append(tile)
-                    #else leave it be
-            #apend col to rows
-            rows.append(col)
-        used.clear()
-        rects = []
-        #join rows to form rects
-        for shape in rows:
-            if shape in used: continue
-            #else this is current shape to attempt and match
-            #used.append(shape)
-            rects.append(shape)
-            for i in range(SEARCH_TIME):
-                for other_shape in rows:
-                    if other_shape in used: continue
-                    #elif above, and width is the same, and x is the same
-                    if other_shape.y == shape.y-other_shape.height and other_shape.width == shape.width and other_shape.x == shape.x:
-                        shape.y -= other_shape.height
-                        shape.height += other_shape.height
-                        other_shape.Del()
-                        used.append(other_shape)
-        
-            
-        # CORRECTION: Make sure generated rectangles are actually added to your active collisions list
-        for r in rects:
-            self.collisions.append(r)
-        print(len(self.collisions))
-        print(len(Global.collisions))
-
+        __gen_collision_shapes(self, COL_FULL, COL_HALF_TOP, COL_HALF_BOTTOM, COL_HALF_LEFT, COL_HALF_RIGHT, TILEMAP_COLLISION_GEN, SEARCH_TIME)
 
     def manual_save_json(self, path=None):
         if path and self.dataFile == None: return
--- src/helpers/_tilemap_files.py
+++ src/helpers/_tilemap_files.py
@@ -2,6 +2,7 @@ import pygame
 import json
 
 from .._net import Global
+from ..physics.collisions import CollisionRect
 
 
 def __saveTileMap_json__(path, data:dict[tuple[int, int ], tuple[int, int]], collisions:dict[tuple[int, int], int]):
@@ -31,4 +32,72 @@ def __loadTileMap_json__(path):
 
 def extract_tuple_from_str(str):
     return tuple(float(x) for x in str[1:-1].split(","))
+
+
+def __gen_collision_shapes(self, COL_FULL, COL_HALF_TOP, COL_HALF_BOTTOM, COL_HALF_LEFT, COL_HALF_RIGHT, TILEMAP_COLLISION_GEN, SEARCH_TIME):
+        os.system("cls")
+        print("Generating collision shapes...")
+        [t.Del() for t in self.collisions]
+        self.collisions.clear()
+        
+        # Separate tiles into shapes
+        full = []
+        
+        for key, value in self.tiles.items():
+            if value not in self.collDef: 
+                continue
+            # CORRECTION: Appending 'key' (grid pos) instead of 'value' (tile type)
+            if self.collDef[value] == COL_FULL:
+                full.append(key)
+
+        # Gen collision shapes
+        _tiles = []
+        used = []
+        rows: list[CollisionRect] = []
+        for pos in full:
+            if pos in used: continue
+            #else creat new collision and run search
+            col = CollisionRect(pos[0]*self.width, pos[1]*self.height, self.width, self.height, self.collisionLayers)
+            #run search through tiles
+            for i in range(SEARCH_TIME):
+                for tile in full:
+                    if tile in used: continue
+                    #else check if it can be added to col shape
+                    #if to the left
+                    if tile[0] == (col.x//self.width)-1 and tile[1] == pos[1]:
+                        col.x -= self.width
+                        col.width += self.width
+                        used.append(tile)
+                    #else check if it is to the right
+                    if tile[0] == (col.x+col.width)//self.width and tile[1] == pos[1]:
+                        col.width += self.width
+                        used.append(tile)
+                    #else leave it be
+            #apend col to rows
+            rows.append(col)
+        used.clear()
+        #join rows to form rects
+        for shape in rows:
+            if shape in used: continue
+            #else this is current shape to attempt and match
+            #used.append(shape)
+            _tiles.append(shape)
+            for i in range(SEARCH_TIME):
+                for other_shape in rows:
+                    if other_shape in used: continue
+                    #elif above, and width is the same, and x is the same
+                    if other_shape.y == shape.y-other_shape.height and other_shape.width == shape.width and other_shape.x == shape.x:
+                        shape.y -= other_shape.height
+                        shape.height += other_shape.height
+                        other_shape.Del()
+                        used.append(other_shape)
+        #half top
+        
+        
+            
+        # CORRECTION: Make sure generated rectangles are actually added to your active collisions list
+        for i in _tiles:
+            self.collisions.append(i)
+        print(len(self.collisions))
+        print(len(Global.collisions))
 
