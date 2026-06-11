ad7f23de1884b0f132fbcf610bfe242dfe6d513b
Sam Fertig <sfertig007@gmail.com>
2026-06-11 14:20:57 -0400

    added smart collision detection to dynamicBody

--- src/physics/dynamicBody.py
+++ src/physics/dynamicBody.py
@@ -3,7 +3,7 @@ from .._net import Global
 from ..helpers.utils import change_layer
 from .collisions import CollisionRect
 class DynamicBody:
-    def __init__(self, x, y, width, height, vx=0.0, vy=0.0, show=True, layer=3, gravity=0.0):
+    def __init__(self, x, y, width, height, vx=0.0, vy=0.0, show=True, layer=3, gravity=0.0, smartCollisions=True, smartCollisionDist=1):
         self.x = x
         self.y = y
         self.vx = vx
@@ -24,11 +24,30 @@ class DynamicBody:
         self.animation = None
         self.animationManager = None
 
+        #collision checks
+        self.smartCollisions = smartCollisions
+        self.smartCollisionDist = smartCollisionDist
+
+        self.is_on_floor = False
+        self.is_on_ceiling = False
+        self.is_on_left = False
+        self.is_on_right = False
+        self.is_on_wall = False
+
+
     def change_layer(self, new_layer):
         change_layer(self, new_layer, self.layer)
         self.layer = new_layer
 
-    def update(self):
+
+    def update(self):  
+
+        #collision checks
+        self.is_on_floor = False
+        self.is_on_ceiling = False
+        self.is_on_left = False
+        self.is_on_right = False
+        self.is_on_wall = False
 
         #x movement
         self.x+=self.vx*Global.dt
@@ -36,17 +55,65 @@ class DynamicBody:
         self.collision.x = self.x
         if self.collision.isColliding():
             self.x-=self.vx*Global.dt
+            #colisions info
+            self.is_on_wall = True
+            if self.vx > 0.0:
+                self.is_on_right = True
+            elif self.vx < 0.0:
+                self.is_on_left = True
+            self.vx = 0
 
-        self.collision.x = self.x
+        if self.smartCollisions:
+            if not self.is_on_left:
+                self.collision.x = self.x - self.smartCollisionDist
+                if self.collision.isColliding():
+                    self.is_on_left = True
+                    self.vx = 0.0
+                self.collision.x = self.x
+
+            if not self.is_on_right:
+                self.collision.x = self.x + self.smartCollisionDist
+                if self.collision.isColliding():
+                    self.is_on_right = True
+                    self.vx = 0.0
+                self.collision.x = self.x
 
         #y movement
-        self.vy+=self.gravity*Global.dt
-        self.y+=self.vy*Global.dt
+        self.y += self.vy*Global.dt
         #if collisions, back up
         self.collision.y = self.y
         if self.collision.isColliding():
             self.y-=self.vy*Global.dt
 
+            #collision info
+            if self.vy > 0.0:
+                self.is_on_floor = True
+            if self.vy < 0.0:
+                self.is_on_ceiling = True
+
+            self.vy = 0
+
+        if self.smartCollisions:
+            if not self.is_on_floor:
+                self.collision.y = self.y + self.smartCollisionDist
+                if self.collision.isColliding():
+                    self.is_on_floor = True
+                    self.vy = 0.0
+                self.collision.y = self.y
+
+            if not self.is_on_ceiling:
+                self.collision.y = self.y - self.smartCollisionDist
+                if self.collision.isColliding():
+                    self.is_on_ceiling = True
+                    self.vy = 0.0
+                self.collision.y = self.y
+
+        #gravity
+        if not self.is_on_floor:
+            self.vy += self.gravity*Global.dt
+
+
+        self.collision.x = self.x
         self.collision.y = self.y
 
         #update any other positions now
