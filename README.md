45fcd132d4d6259c76a0ec255d1c106d09faef4f
Sam Fertig <sfertig007@gmail.com>
2026-06-23 12:24:41 -0400

    full engine rewrite to upgrade out of framework

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-v 1.1.3
-Updates: 
-    -Fixed left over collision shapes in change and reload scene
+still in developement
     
     
--- src/devPace/assets/text.py
+++
@@ -1,7 +0,0 @@
-import pygame
-from .._net import Global
-from .cache import Assets
-
-class Text:
-    def __init__(self, text, x, y, size, color, font=None, bold=False, italic=False, underline=False):
-        pass
